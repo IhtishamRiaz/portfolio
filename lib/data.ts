7afbadcc29937b52a3fc944a5942e4500db05754
Muhammad Ihtisham <riaz4u72nb+github@gmail.com>
2024-01-18 01:14:37 +0500

    shareit added

--- lib/data.ts
+++ lib/data.ts
@@ -5,6 +5,7 @@ import { LuGraduationCap } from "react-icons/lu";
 import corpcommentImg from "@/public/corpcomment.png";
 import rmtdevImg from "@/public/rmtdev.png";
 import wordanalyticsImg from "@/public/wordanalytics.png";
+import shareIt from "@/public/share-it.png";
 
 export const links = [
    {
@@ -62,11 +63,11 @@ export const experiencesData = [
 
 export const projectsData = [
    {
-      title: "CorpComment",
+      title: "Share it",
       description:
-         "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
-      tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
-      imageUrl: corpcommentImg,
+         "ShareIt is a secure file-sharing web app, allowing users to effortlessly upload files to the cloud, set passwords, and share via links or email.",
+      tags: ["React", "Next.js", "Firebase", "Tailwind", "Clerk"],
+      imageUrl: shareIt,
    },
    {
       title: "rmtDev",
